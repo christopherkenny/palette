a8e9c5d21f424a7596dfdd9fbce0c850efb887e9
Christopher Kenny <ctk44@cornell.edu>
2024-03-11 16:38:19 -0400

    add pal fn

---
+++ R/palette_function.R
@@ -0,0 +1,28 @@
+#' Build a palette function
+#'
+#' Creates a palette function for use within `ggplot2` as an argument to `discrete_scale`.
+#'
+#' @param x a palette
+#'
+#' @return a function which takes an integer `n` and returns a vector of `n` colors
+#' @export
+#'
+#' @examples
+#' palette_function(palette(roygbiv))(10)
+palette_function <- function(x) {
+
+  #if (type == 'discrete') {
+    pal <- unname(x)
+    function(n) {
+      if (n <= length(pal)) {
+        pal[seq_len(n)]
+      } else {
+        rep(pal, ceiling(n / length(pal)))[seq_len(n)]
+      }
+    }
+  # } else if (type %in% c('continuous', 'binned')) {
+  #
+  # } else {
+  #   cli::cli_abort('{.arg type} must be one of {.val discrete}, "{.val continuous}, or {.val binned}.')
+  # }
+}
